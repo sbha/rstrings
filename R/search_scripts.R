a44bf362c7b2d49e2ac12d2aeb51b1aec1b34b97
sbha <sbharty@gmail.com>
2018-10-30 20:55:07 -0500

    search_scripts update

--- R/search_scripts.R
+++ R/search_scripts.R
@@ -10,26 +10,35 @@
 #'
 #' @export search_scripts
 
-search_scripts <- function(file_name, search_string){
-  read_file <- function(file_name){
-    con <- file(file_name, open="r")
-    on.exit(close(con))
-    # warn is included because some scripts do not end with an EOL character 
-    readLines(con, warn = FALSE)
+search_scripts <- function(dir_path, search_string, file_ext = '\\.(R|r)$'){
+  search_script <- function(file_name, search_string){
+    read_file <- function(file_name){
+      con <- file(file_name, open="r")
+      on.exit(close(con))
+      # warn is included because some scripts do not end with an EOL character 
+      readLines(con, warn = FALSE)
+    }
+    
+    check_string <- function(file_name, search_string){
+      file <- read_file(file_name)
+      cnt <- length(grep(search_string, file))
+      dir_path <- gsub('(^.+/)(.+\\.R$)', '\\1', file_name)
+      file_name <- gsub('(^.+/)(.+\\.R$)', '\\2', file_name)
+      df <- data.frame(directory = dir_path, file_name = file_name, pattern_count = cnt, stringsAsFactors = FALSE)
+      df
+    }
+    
+    df <- check_string(file_name, search_string)
+    
+    return(df)
   }
   
-  check_string <- function(file_name, search_string){
-    file <- read_file(file_name)
-    cnt <- length(grep(search_string, file))
-    dir_path <- gsub('(^.+/)(.+\\.R$)', '\\1', file_name)
-    file_name <- gsub('(^.+/)(.+\\.R$)', '\\2', file_name)
-    df <- data.frame(directory = dir_path, file_name = file_name, pattern_count = cnt, stringsAsFactors = FALSE)
-    df
-  }
+  # aggregate all scripts in directory
+  df_out <- list.files(dir_path, pattern = file_ext, full.names = TRUE, recursive = TRUE) %>%
+    purrr::map_df(~search_script(., search_string)) %>%
+    dplyr::filter(pattern_count > 0) %>%
+    dplyr::tbl_df()
   
-  df <- check_string(file_name, search_string)
+  return(df_out)
   
-  return(df)
 }
-
-
