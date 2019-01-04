6e0e9ec3dabf26b1a2376af2b14166e4cb534230
“sbha” <sbharty@gmail.com>
2019-01-04 14:40:40 -0600

    binding does not work with strings that end with a period

--- R/str_replacer.R
+++ R/str_replacer.R
@@ -14,7 +14,8 @@ str_replacer <- function(str_target, to_replace, replace_with){
   library(stringr)
   to_replace <- as.character(to_replace)
   replace_with <- as.character(replace_with)
-  to_replace <- paste0('\\b', to_replace, '\\b')
+  to_replace <- paste0('\\b', to_replace)
+  to_replace <- ifelse(stringr::str_detect(to_replace, '\\.$'), paste0(to_replace, '\\B'), paste0(to_replace, '\\b'))
   names(replace_with) <- to_replace
   str_out <- str_replace_all(str_target, replace_with)
   return(str_out)
