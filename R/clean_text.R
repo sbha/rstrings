748f13d861bed202c4615109af673dd78910b643
“sbha” <sbharty@gmail.com>
2020-10-01 21:07:46 -0500

    renaming function and adding argument options for different types of string cleaning

--- R/clean_text.R
+++ R/clean_text.R
@@ -1,10 +1,10 @@
-clean_text <- function(x){
-  x <- tolower(x)
-  
+clean_str <- function(x, rm_case = FALSE, rm_punct = FALSE, rm_num = FALSE, ...){
+  if (isTRUE(rm_case)) x <- tolower(x)
+  if (isTRUE(rm_punct)) x <- gsub('[[:punct:] ]', ' ', x)
+  if (isTRUE(rm_num)) x <- gsub('\\d+', ' ', x)
+  #if (hasArg(rm_tokens)) x <- str_replace_all(x, rm_tokens, ' ')
   x <- gsub('\\s+', ' ', x)
-  
   x <- trimws(x)
-  
   x
-  
 }
+
