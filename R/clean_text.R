4c7befd3324dbfeb90ece70392bd306bc668c989
“sbha” <sbharty@gmail.com>
2021-08-26 20:32:28 -0500

    function to clean up messy text with option to remove select tokens

--- R/clean_text.R
+++ R/clean_text.R
@@ -1,8 +1,8 @@
-clean_str <- function(x, rm_case = FALSE, rm_punct = FALSE, rm_num = FALSE, ...){
+clean_str <- function(x, rm_case = TRUE, rm_punct = TRUE, rm_num = FALSE, rm_tokens = NULL){
   if (isTRUE(rm_case)) x <- tolower(x)
   if (isTRUE(rm_punct)) x <- gsub('[[:punct:] ]', ' ', x)
   if (isTRUE(rm_num)) x <- gsub('\\d+', ' ', x)
-  #if (hasArg(rm_tokens)) x <- str_replace_all(x, rm_tokens, ' ')
+  if (hasArg(rm_tokens)) x <- gsub(search_binder(rm_tokens), ' ', x)
   x <- gsub('\\s+', ' ', x)
   x <- trimws(x)
   x
