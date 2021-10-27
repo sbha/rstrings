733b95f3c5eb7ca515e9f8ca40afd080f8cfef11
“sbha” <sbharty@gmail.com>
2021-10-26 19:56:43 -0500

    namespace clean up

--- R/highlighter.R
+++ R/highlighter.R
@@ -12,10 +12,10 @@
 
 highlighter <- function(txt, substring){
   if (grepl(substring, txt, fixed = TRUE)){
-    highlight_substring <- crayon::red$bold(substring)
+    highlight_substring <- red$bold(substring)
     cat(unlist(strsplit(paste(txt, ''), substring)), sep = highlight_substring)
   } else {
-    highlight_substring <- crayon::red$bold(substring)
+    highlight_substring <- red$bold(substring)
     cat(paste0("the substring '", highlight_substring, "' does not exist in the target string"))
   }
 }
