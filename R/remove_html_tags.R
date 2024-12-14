452e381af390dd00860948a36cfac3ee38b26751
sbha <sbharty@gmail.com>
2024-12-14 14:38:51 -0600

    remove_html_tags

---
+++ R/remove_html_tags.R
@@ -0,0 +1,21 @@
+# '<.*?>'
+
+remove_html_tags <- function(x, 
+                        replace_with = ' ', 
+                        trim = TRUE,
+                        squish = TRUE,
+                        re_tags = '<[^>]*>'){
+  
+  cs <- str_replace_all(x, re_tags, replace_with)
+  
+  if (trim){
+    cs <- str_trim(cs)
+  } 
+  
+  if (squish){
+    cs <- str_squish(cs)
+  } 
+  
+  cs
+  
+}
